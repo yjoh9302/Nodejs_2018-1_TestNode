14e5426fdafabc717cea7fbbf44e30f04baaa2aa
yjoh9302 <ohyj48@naver.com>
2018-04-17 10:25:36 +0900

    test commit

--- commit_push.bat
+++ commit_push.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "first commit"
+git commit -m "test commit"
 git push -u origin master
--- index.js
+++ index.js
@@ -1,4 +1,1 @@
 console.log(typeof console === 'object');
-console.log('Node.js version'+process.version);
-console.log(process.memoryUsage());
-console.log(process.env);
